ac3f2e2cd596702f838da7a8c01f7413ac97a3f1
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-07-09 12:40:56 -0300

    add delete button

--- index.js
+++ index.js
@@ -2,15 +2,29 @@
 //let usr_ipt = document.GetElementById("input-el")
 //let titulo = document.GetElementById("input-button")
 
-let myLeads = []
+let myLeads = [""]
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage=JSON.parse( localStorage.getItem("myLeads"))
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
